3c6da9a48e9668aad1f86cf8fef96a0fe65bfb76
its-prithi31 <rprithika31@gmail.com>
2024-08-12 07:56:54 +0530

    Add files via upload

---
+++ AdditionProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AdditionProgram{
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(" ");
+        int firstNumber=scanner.nextInt();
+        System.out.println(" ");
+        int secondNumber=scanner.nextInt();
+        int sum=firstNumber+secondNumber;
+        System.out.println(sum);
+    }
+    
+}
